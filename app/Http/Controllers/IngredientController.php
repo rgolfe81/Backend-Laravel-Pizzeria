40eb033a99d789604b8b55503b125560323b3754
Ruben <rgolfe81@gmail.com>
2023-04-04 23:41:20 +0200

    feat: request update ingredient in IngredientController

--- app/Http/Controllers/IngredientController.php
+++ app/Http/Controllers/IngredientController.php
@@ -86,4 +86,71 @@ class IngredientController extends Controller
             );
         }
     }
+
+    public function updateIngredient(Request $request, $id)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required | regex:/^[A-Za-z0-9]+$/',
+                'type' => 'required | regex:/^[A-Za-z0-9]+$/',
+                'quantity' => 'required | numeric',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+
+            $ingredient = Ingredient::find($id);
+
+            if (!$ingredient) {
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "Ingredient doesn't exists",
+                    ],
+                    404
+                );
+            }
+
+            $name = $request->input('name');
+            $type = $request->input('type');
+            $quantity = $request->input('quantity');
+
+            // $ingredient->name = $request->input('name');
+            // $ingredient->type = $request->input('type');
+            // $ingredient->price = $request->input('quantity');
+
+            if (isset($name)) {
+                $ingredient->name = $name;
+            }
+
+            if (isset($type)) {
+                $ingredient->type = $type;
+            }
+
+            if (isset($price)) {
+                $ingredient->quantity = $quantity;
+            }
+
+            $ingredient->save();
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Ingredient updated",
+                    "data" => $ingredient
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => $th->getMessage()
+                ],
+                500
+            );
+        }
+    }
+
 }
